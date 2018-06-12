6a3dfde4b10f5179716a02a7c00416ec7f032e94
Steffen Eitelmann <steffen.eitelmann@unreadable.eu>
2018-06-12 18:50:50 +0200

    Conflict solved

--- WeDontGiveAShip/src/com/weDontGiveAShip/UI/Gui.java
+++ WeDontGiveAShip/src/com/weDontGiveAShip/UI/Gui.java
@@ -1,45 +1,4 @@
-<<<<<<< HEAD
-package com.weDontGiveAShip.UI;
 
-import javax.swing.JFrame;
-
-import com.weDontGiveAShip.main.Main;
-
-public class Gui extends JFrame{
-
-	private static final long serialVersionUID = 1L;
-	
-	public Gui() {
-		super("Battleship by We Don't Give A Ship");
-		setSize(600, 800);
-		setResizable(false);
-		setLocationRelativeTo(null);
-		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		
-		drawMainMenu();
-		
-		setVisible(true);
-	}
-
-	private void drawMainMenu() {
-		MainMenuPanel mainMenu = new MainMenuPanel();
-		add(mainMenu);
-	}
-
-	public void openShipPlacer() {
-		clear();
-		
-		
-		
-	}
-
-	public static void clear(){
-		Main.gui.getContentPane().removeAll();
-		Main.gui.repaint();
-	}
-	
-}
-=======
 package com.weDontGiveAShip.UI;
 
 import javax.swing.JFrame;
@@ -83,4 +42,4 @@ public class Gui extends JFrame{
 	}
 	
 }
->>>>>>> refs/heads/ShipPlacer
+
